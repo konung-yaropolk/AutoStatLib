Bugfix: SD uses population std ddof=0 instead of sample std ddof=1

src/AutoStatLib/StatPlots.py
@@ -145,8 +145,8 @@ class BaseStatPlot(Helpers):
         self.median = [
             np.median(self.data_groups[i]).item() for i in range(self.n_groups)]
         self.sd = [
-            np.std(self.data_groups[i]).item() for i in range(self.n_groups)]
-        self.sem = [np.std(self.data_groups[i]).item() / np.sqrt(len(self.data_groups[i])).item()
+            np.std(self.data_groups[i], ddof=1).item() for i in range(self.n_groups)]
+        self.sem = [np.std(self.data_groups[i], ddof=1).item() / np.sqrt(len(self.data_groups[i])).item()
                     for i in range(self.n_groups)]
 
         self.n = [len(i) for i in self.data_groups]

src/AutoStatLib/helpers.py
@@ -79,8 +79,8 @@ class Helpers():
             'Groups_N': [len(self.data[i]) for i in range(len(self.data))],
             'Groups_Median': [np.median(self.data[i]).item() for i in range(len(self.data))],
             'Groups_Mean': [np.mean(self.data[i]).item() for i in range(len(self.data))],
-            'Groups_SD': [np.std(self.data[i]).item() for i in range(len(self.data))],
-            'Groups_SE': [np.std(self.data[i]).item() / np.sqrt(len(self.data[i])) for i in range(len(self.data))],
+            'Groups_SD': [np.std(self.data[i], ddof=1).item() for i in range(len(self.data))],
+            'Groups_SE': [np.std(self.data[i], ddof=1).item() / np.sqrt(len(self.data[i])) for i in range(len(self.data))],
             'subgrouping': self.subgrouping,
             # actually returns list of lists of numpy dtypes of float64, next make it return regular floats:
             'Samples': self.data,